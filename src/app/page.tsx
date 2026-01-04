4e3a4b379873703617dc8384bbd5fd2397ed5efc
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-01-04 15:14:12 -0500

    feat: config main page to use api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,33 @@
-import Image from "next/image";
+'use client';
+
+import { useEffect, useState } from "react";
+import { getProducts } from "./api/products";
+import { ProductCard } from "@/components/ui/molecules/ProductCard";
+import { ProductCardProps } from "@/types/product.type";
 
 export default function Home() {
+  const [products, setProducts] = useState<ProductCardProps[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getProducts();
+      setProducts(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="font-sans flex items-center justify-center h-full p-8 pb-10 sm:p-4">
-      <main className="flex flex-col items-center sm:items-start">
-        <div className="card bg-base-100 w-96 shadow-sm">
-          <figure>
-            <Image
-              src="/next-mart.png"
-              alt="Next.js logo"
-              width={100}
-              height={100}
-            />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Card Title</h2>
-            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Buy Now</button>
-            </div>
-          </div>
-        </div>
+      <main className="flex flex-wrap justify-center items-center gap-6 sm:items-start">
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            image={product.image}
+            price={product.price}
+          />
+        ))}
       </main>
     </div>
   );
