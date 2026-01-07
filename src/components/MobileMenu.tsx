a0b91548c55b4557ebd45f36bcba367069b3dc8a
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-01-07 11:21:35 -0500

    feat: change design in mobile header

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -24,13 +24,24 @@ export const MobileMenu = ({ session }: MobileMenuProps) => {
   ];
 
   return (
-    <div className="bg-white md:hidden">
+    <div className="flex justify-center items-center md:hidden">
+      <div className="flex gap-3 items-center">
+        {session?.user?.image && (
+          <Image
+            src={session.user.image}
+            alt={session.user.name ?? "User"}
+            width={24}
+            height={24}
+            className="rounded-full"
+          />
+        )}
+      </div>
       <button onClick={toggleMobileMenu} className="p-2">
-        <Menu size={32} />
+        <Menu size={24} />
       </button>
 
       {openMobile && (
-        <div className="absolute top-20 left-0 w-full bg-white shadow-lg p-6 flex flex-col gap-6 z-50">
+        <div className="absolute top-20 left-0 w-full bg-white shadow-lg p-6 flex flex-col gap-6 z-50 text-center">
           <div className="flex flex-col gap-4">
             {links.map((link) => (
               <Link
@@ -44,22 +55,9 @@ export const MobileMenu = ({ session }: MobileMenuProps) => {
             ))}
           </div>
 
-          <div className="divider my-0"></div>
-
           <div className="flex justify-between items-center">
             {session?.user ? (
-              <div className="flex w-full justify-between items-center gap-4">
-                <div className="flex gap-3 items-center">
-                  {session.user.image && (
-                    <Image
-                      src={session.user.image}
-                      alt={session.user.name ?? "User"}
-                      width={32}
-                      height={32}
-                      className="rounded-full"
-                    />
-                  )}
-                </div>
+              <div className="flex w-full justify-center items-center gap-4">
                 <SignOut
                   className="cursor-pointer"
                 />
